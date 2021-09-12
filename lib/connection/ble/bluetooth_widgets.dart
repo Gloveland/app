6d980dc966a8e2f47a8a7635a81430ddf6fe2a27
Jazmin Ferreiro <jazminsofiaf@gmail.com>
2021-09-12 11:42:28 -0300

    change to statefull widget

--- lib/connection/ble/bluetooth_widgets.dart
+++ lib/connection/ble/bluetooth_widgets.dart
@@ -1,6 +1,4 @@
 import 'dart:async';
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import 'package:lsa_gloves/datacollection/storage.dart';
@@ -38,29 +36,105 @@ class ServiceTile extends StatelessWidget {
   }
 }
 
-class CharacteristicTile extends StatelessWidget {
+
+/// This is the stateful widget that the main application instantiates.
+class CharacteristicTile extends StatefulWidget {
   final BluetoothCharacteristic characteristic;
   final String deviceId;
-  late final List<DescriptorTile> descriptorTiles;
 
+  CharacteristicTile({Key? key, required this.deviceId, required this.characteristic})
+      : super(key: key);
+
+  @override
+  State<CharacteristicTile> createState() =>
+      _BLEMovementRecorderWidget(this.deviceId, this.characteristic, false);
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _BLEMovementRecorderWidget extends State<CharacteristicTile> {
+  bool _isRecording;
+  final BluetoothCharacteristic characteristic;
+  final String deviceId;
+  final GlobalKey<State> _keyLoader = new GlobalKey<State>();
   StreamController<Movement> _streamController;
   List<Movement> _items;
-  final GlobalKey<State> _keyLoader = new GlobalKey<State>();
 
-
-
-  CharacteristicTile({Key? key, required this.deviceId, required this.characteristic}) :
+  _BLEMovementRecorderWidget(this.deviceId, this.characteristic, this._isRecording) :
         _streamController = new StreamController.broadcast(),
-        _items = [],
-        super(key: key){
-    this.descriptorTiles = characteristic.descriptors.map(
-          (descriptor) => DescriptorTile(
-        descriptor: descriptor
-      ),
-    ).toList();
+        _items = [] {
+    _streamController = new StreamController.broadcast();
   }
 
-  _readGloveMeasurements(List<int> valueRead) {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<List<int>>(
+      stream: characteristic.value,
+      initialData: characteristic.lastValue,
+      builder: (c, snapshot) {
+        final value = snapshot.data;
+        readGloveMeasurementsFromBle(value!);
+        return ExpansionTile(
+            title: ListTile(
+              title: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Characteristic'),
+                  Text('${characteristic.uuid.toString()}',
+                      style: Theme.of(context).textTheme.body1?.copyWith(
+                          color: Theme.of(context).textTheme.caption?.color))
+                ],
+              ),
+            ),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(
+                      characteristic.isNotifying
+                          ? Icons.sync_disabled
+                          : Icons.sync,
+                      color: Theme.of(context).iconTheme.color?.withOpacity(0.5)),
+                  onPressed: () async {
+                    await characteristic.setNotifyValue(!characteristic.isNotifying);
+                    await characteristic.read();
+                  },
+                )
+              ],
+            )
+        );
+      },
+    );
+  }
+
+
+  /*
+
+  VoidCallback? startRecording() {
+    print('startRecording');
+    setState(() {
+      _isRecording = true;
+    });
+    print('streamController');
+    _streamController = new StreamController.broadcast();
+    _streamController.stream.listen((p) => {setState(() => _items.add(p))});
+    print('load msg from connection into item list');
+  }
+
+  VoidCallback? stopRecording() {
+    print('stopRecording');
+    _streamController.close();
+    setState(() {
+      _isRecording = false;
+    });
+    if (_items.isNotEmpty) {
+      saveMessagesInFile("PALABRA", this._items);
+    }
+  }
+   */
+
+
+  readGloveMeasurementsFromBle(List<int> valueRead) {
     String stringRead = new String.fromCharCodes(valueRead);
     print("READING.... $stringRead");
     if(stringRead.isEmpty){
@@ -89,77 +163,12 @@ class CharacteristicTile extends StatelessWidget {
 
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<List<int>>(
-      stream: characteristic.value,
-      initialData: characteristic.lastValue,
-      builder: (c, snapshot) {
-        final value = snapshot.data;
-        return ExpansionTile(
-          title: ListTile(
-            title: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('Characteristic'),
-                Text('${characteristic.uuid.toString()}',
-                    style: Theme.of(context).textTheme.body1?.copyWith(
-                        color: Theme.of(context).textTheme.caption?.color))
-              ],
-            ),
-            subtitle: Text(_readGloveMeasurements(value!).toString()),
-            contentPadding: EdgeInsets.all(0.0),
-          ),
-          trailing: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              IconButton(
-                icon: Icon(
-                  Icons.file_download,
-                  color: Theme.of(context).iconTheme.color?.withOpacity(0.5),
-                ),
-                onPressed: () async {
-                  await characteristic.read();
-                },
-              ),
-              IconButton(
-                icon: Icon(Icons.file_upload,
-                    color: Theme.of(context).iconTheme.color?.withOpacity(0.5)),
-                onPressed: () async {
-                  await characteristic.write(
-                      utf8.encode("on write characteristic"),
-                      withoutResponse: true);
-                  await characteristic.read();
-                },
-              ),
-              IconButton(
-                icon: Icon(
-                    characteristic.isNotifying
-                        ? Icons.sync_disabled
-                        : Icons.sync,
-                    color: Theme.of(context).iconTheme.color?.withOpacity(0.5)),
-                onPressed: () async {
-                  await characteristic
-                      .setNotifyValue(!characteristic.isNotifying);
-                  await characteristic.read();
-                },
-              )
-            ],
-          ),
-          children: descriptorTiles,
-        );
-      },
-    );
-  }
-
-  saveMessagesInFile(String fileName, List<Movement> movements) async {
+  saveMessagesInFile(String word, List<Movement> movements) async {
     if (movements.isEmpty) {
       return;
     }
     //open pop up loading
-   // Dialogs.showLoadingDialog(context, _keyLoader, "Guardando...");
-    var word = fileName;
+    //Dialogs.showLoadingDialog(context, _keyLoader, "Guardando...");
     var deviceId = movements.first.deviceId;
     var measurementFile = await DeviceMeasurementsFile.create(deviceId, word);
     for (int i = 0; i < movements.length; i++) {
@@ -170,80 +179,5 @@ class CharacteristicTile extends StatelessWidget {
     this._items = [];
     //close pop up loading
     Navigator.of(_keyLoader.currentContext!, rootNavigator: true).pop();
-  }
-
-}
-
-class DescriptorTile extends StatelessWidget {
-  final BluetoothDescriptor descriptor;
-
-  const DescriptorTile(
-      {Key? key,
-      required this.descriptor})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text('Descriptor'),
-          Text('0x${descriptor.uuid.toString().toUpperCase().substring(4, 8)}',
-              style: Theme.of(context)
-                  .textTheme
-                  .body1
-                  ?.copyWith(color: Theme.of(context).textTheme.caption?.color))
-        ],
-      ),
-      subtitle: StreamBuilder<List<int>>(
-        stream: descriptor.value,
-        initialData: descriptor.lastValue,
-        builder: (c, snapshot) => Text(snapshot.data.toString()),
-      ),
-      trailing: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.file_download,
-              color: Theme.of(context).iconTheme.color?.withOpacity(0.5),
-            ),
-            onPressed: () => descriptor.read(),
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.file_upload,
-              color: Theme.of(context).iconTheme.color?.withOpacity(0.5),
-            ),
-            onPressed: () => descriptor.write(utf8.encode("on write descriptor"))
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class AdapterStateTile extends StatelessWidget {
-  const AdapterStateTile({Key? key, required this.state}) : super(key: key);
-
-  final BluetoothState state;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.redAccent,
-      child: ListTile(
-        title: Text(
-          'Bluetooth adapter is ${state.toString().substring(15)}',
-          style: Theme.of(context).primaryTextTheme.subhead,
-        ),
-        trailing: Icon(
-          Icons.error,
-          color: Theme.of(context).primaryTextTheme.subhead?.color,
-        ),
-      ),
-    );
   }
 }
