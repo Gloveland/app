7033075abb72e993a6e49e488443d0bf7414b3f1
Darius Maitia <dariusmaitia@gmail.com>
2021-10-09 23:33:55 -0300

    Refactor BluetoothBackend + Devices screen
    The devices screen and the bluetooth backend get modified in order
    to make that the devices displayed depend on the backend's state,
    making that the connected devices get displayed automatically
    without the need of scanning the devices. This is not the case
    for devices that are available but not connected, to display them
    we must scan the devices, unless the device now disconnected was
    previously displayed as connected (in case the user disconnected
    it).

--- lib/connection/ble/bluetooth_backend.dart
+++ lib/connection/ble/bluetooth_backend.dart
@@ -34,18 +34,30 @@ class BluetoothBackend with ChangeNotifier {
   Map<BluetoothDevice, BluetoothCharacteristic>
       get interpretationCharacteristics => _interpretationCharacteristics;
 
+  /// Stream providing the information of the connected devices. This stream
+  /// monitors the connected devices every 2 seconds.
   late Stream<List<BluetoothDevice>> connectedDevicesStream;
 
+  late Stream<List<BluetoothDevice>> availableDevicesStream;
+
   BluetoothBackend() {
+    _initializeStreams();
     _startMonitoringDevices();
+  }
+
+  void _initializeStreams() {
+    availableDevicesStream = FlutterBlue.instance.scanResults.map((list) => list
+        .where((scanResult) => scanResult.advertisementData.connectable)
+        .map((scanResult) => scanResult.device)
+        .toList(growable: true));
+    this.connectedDevicesStream = Stream.periodic(Duration(seconds: 2))
+        .asyncMap((_) => BluetoothBackend.getConnectedDevices())
+        .asBroadcastStream();
   }
 
   /// Starts monitoring the connected devices and notifies the listeners of this
   /// class when a connection event (i.e. connection or disconnection) happens.
   void _startMonitoringDevices() {
-    this.connectedDevicesStream = Stream.periodic(Duration(seconds: 2))
-        .asyncMap((_) => BluetoothBackend.getConnectedDevices())
-        .asBroadcastStream();
     this.connectedDevicesStream.listen((newConnectedDevices) async {
       if (this._connectedDevices.length != newConnectedDevices.length) {
         developer.log("Connection event.", name: TAG);
