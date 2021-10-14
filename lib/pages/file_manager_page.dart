47f01ce282d2bc5a5bf9a7ca35982e749c2ec740
Jazmin Ferreiro <jazminsofiaf@gmail.com>
2021-10-13 23:40:58 -0300

    delete unused page

--- lib/pages/file_manager_page.dart
+++ lib/pages/file_manager_page.dart
@@ -7,88 +7,6 @@ import 'package:path/path.dart';
 import 'package:flutter/material.dart';
 import 'package:lsa_gloves/datacollection/storage.dart';
 
-class OldFileManagerPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Gestion de archivos"),
-      ),
-      drawer: NavDrawer(),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            StreamBuilder<List<DeviceMeasurementsFile>>(
-              stream: GloveEventsStorage().getListOfFiles().asStream(),
-              initialData: [],
-              builder: (c, snapshot) => Column(
-                children: snapshot.data!
-                    .map(
-                      (f) => Card(
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: <Widget>[
-                            ListTile(
-                              leading: IconButton(
-                                icon: Icon(Icons.folder_open_sharp),
-                                onPressed: () async {
-                                  Navigator.pushNamed(
-                                    context,
-                                    FileContentPage.routeName,
-                                    arguments: f,
-                                  );
-                                },
-                              ),
-                              title: Text(basename(f.path)),
-                              subtitle: Text(f.lastModified),
-                              trailing: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: <Widget>[
-                                  IconButton(
-                                    icon: Icon(Icons.delete_outline_outlined,
-                                        color: Theme.of(context)
-                                            .iconTheme
-                                            .color
-                                            ?.withOpacity(0.5)),
-                                    onPressed: () async {
-                                      await f.deleteFile();
-                                      //refresh
-                                    },
-                                  ),
-                                  IconButton(
-                                    icon: Icon(
-                                      Icons.file_upload,
-                                      color: Theme.of(context)
-                                          .iconTheme
-                                          .color
-                                          ?.withOpacity(0.5),
-                                    ),
-                                    onPressed: () async {
-                                      await f.upload();
-                                      Navigator.pushNamed(
-                                        context,
-                                        FileContentPage.routeName,
-                                        arguments: f,
-                                      );
-                                    },
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    )
-                    .toList(),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
 class FileManagerPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -103,6 +21,7 @@ class _FileManagerPage extends State<FileManagerPage> {
         appBar: AppBar(
           title: Text("Gestion de archivos"),
         ),
+        drawer: NavDrawer(),
         body: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
